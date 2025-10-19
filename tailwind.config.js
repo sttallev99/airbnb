2b630fac77d8590d06b974c1d6f2d336593b18dd
sttallev99 <sttallev99@gmail.com>
2025-10-19 12:31:11 +0300

    Add and customize navigation

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        primary: {
+          DEFAULT: "#FF5A5F",
+          dark: "#FF385C",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
